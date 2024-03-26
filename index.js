dbf1c785c27de7aa717cfdeae378723df7c56d4e
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-03-26 09:01:09 +0300

    ARRRAY

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record){
+    const result = record.find(record => record.result === "W" );
+    return !!result ? result.year : undefined
+}
+//TENARY OPERATOR
